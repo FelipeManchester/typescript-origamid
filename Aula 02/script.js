5d193b35e74a92a511b5d4f8bbb57f919463c376
Felipe Manchester <f.p2809@gmail.com>
2023-01-25 12:53:17 -0300

    lesson 0204

--- Aula 02/script.js
+++ Aula 02/script.js
@@ -9,3 +9,17 @@ const toNumber = (value) => {
     throw 'valor deve ser number ou string';
 };
 console.log(toNumber(20));
+async function fetchProduct() {
+    const res = await fetch('https://api.origamid.dev/json/notebook.json');
+    const data = await res.json();
+    showProduct(data);
+}
+fetchProduct();
+function showProduct(data) {
+    document.body.innerHTML = `
+  <div>
+    <h2>${data.nome}</h2>
+    <p>${data.preco}</p>
+  </div>
+  `;
+}
--- Aula 02/script.ts
+++ Aula 02/script.ts
@@ -9,3 +9,38 @@ const toNumber = (value: number | string) => {
 
 
 console.log(toNumber(20))
+
+// Exercicio API
+
+type Empresa = {
+  nome: string;
+  fundacao: number;
+  pais: string;
+}
+
+type Data = {
+  nome: string;
+  preco: number;
+  descricao: string;
+  garantia: string;
+  seguroAcidentes: boolean;
+  empresaFabricante: Empresa
+  empresaMontadora: Empresa
+}
+
+async function fetchProduct() {
+  const res = await fetch('https://api.origamid.dev/json/notebook.json');
+  const data = await res.json();
+  showProduct(data)
+}
+
+fetchProduct();
+
+function showProduct(data: Data) {
+  document.body.innerHTML = `
+  <div>
+    <h2>${data.nome}</h2>
+    <p>${data.preco}</p>
+  </div>
+  `
+}

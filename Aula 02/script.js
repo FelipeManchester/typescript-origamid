8bdab994e98f3a85bd1c37daf684cd0b31f30d62
Felipe Manchester <f.p2809@gmail.com>
2023-01-25 13:46:51 -0300

    lesson 0205

--- Aula 02/script.js
+++ Aula 02/script.js
@@ -18,8 +18,40 @@ fetchProduct();
 function showProduct(data) {
     document.body.innerHTML = `
   <div>
+    <h1>Exercicio API 1</h1>
     <h2>${data.nome}</h2>
     <p>${data.preco}</p>
+    <hr />
   </div>
   `;
+}
+async function fetchCursos() {
+    const res = await fetch('https://api.origamid.dev/json/cursos.json');
+    const data = await res.json();
+    showCourses(data);
+}
+fetchCursos();
+function showCourses(cursos) {
+    document.body.innerHTML += `
+  <h1>Exercicio API 2</h1>
+  `;
+    cursos.forEach(curso => {
+        let color;
+        if (curso.nivel === 'iniciante') {
+            color = 'blue';
+        }
+        else if (curso.nivel === 'avancado') {
+            color = 'red';
+        }
+        document.body.innerHTML += `
+    <div>
+      <h2 style='color: ${color}'>${curso.nome}</h2>
+      <p>Horas: ${curso.horas}</p>
+      <p>Aulas: ${curso.aulas}</p>
+      <p>Tipo: ${curso.gratuito ? 'Gratuito' : 'Pago'}</p>
+      <p>Tags: ${curso.tags.join(', ')}</p>
+      <p>Id: ${curso.idAulas.join(' - ')}</p>
+    </div>
+    `;
+    });
 }
--- Aula 02/script.ts
+++ Aula 02/script.ts
@@ -39,8 +39,56 @@ fetchProduct();
 function showProduct(data: Data) {
   document.body.innerHTML = `
   <div>
+    <h1>Exercicio API 1</h1>
     <h2>${data.nome}</h2>
     <p>${data.preco}</p>
+    <hr />
   </div>
   `
+}
+
+
+// Exercicio API 2
+
+type Cursos = {
+  nome: string;
+  aulas: number;
+  gratuito: boolean;
+  horas: number;
+  idAulas: number[];
+  nivel: 'iniciante' | 'avancado';
+  tags: string[];
+}
+
+async function fetchCursos() {
+  const res = await fetch ('https://api.origamid.dev/json/cursos.json');
+  const data = await res.json();
+  showCourses(data);
+}
+
+fetchCursos();
+
+function showCourses(cursos: Cursos[]) {
+  document.body.innerHTML += `
+  <h1>Exercicio API 2</h1>
+  `
+  cursos.forEach(curso => {
+    let color;
+    if(curso.nivel === 'iniciante') {
+      color = 'blue'
+    } else if (curso.nivel === 'avancado') {
+    color = 'red'
+  }
+
+    document.body.innerHTML += `
+    <div>
+      <h2 style='color: ${color}'>${curso.nome}</h2>
+      <p>Horas: ${curso.horas}</p>
+      <p>Aulas: ${curso.aulas}</p>
+      <p>Tipo: ${curso.gratuito ? 'Gratuito' : 'Pago'}</p>
+      <p>Tags: ${curso.tags.join(', ')}</p>
+      <p>Id: ${curso.idAulas.join(' - ')}</p>
+    </div>
+    `
+  })
 }

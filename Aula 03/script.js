7b1cc38f457802d04f928767237666c280d2c79c
Felipe Manchester <f.p2809@gmail.com>
2023-01-26 14:01:37 -0300

    lesson 0302

--- Aula 03/script.js
+++ Aula 03/script.js
@@ -1,4 +1,15 @@
 "use strict";
+// EXERCICIO 1
 const link = document.getElementById("origamid");
 link instanceof HTMLAnchorElement ? link.href = link.href.replace('http', 'https') : '';
 console.log(link);
+// EXERCÍCIO 2
+const links = document.querySelectorAll('.link');
+const changeLinkStyle = () => {
+    const linksArray = Array.from(links);
+    linksArray.forEach((link) => {
+        link.style.color = 'red';
+        link.style.border = '2px dotted tomato';
+    });
+};
+changeLinkStyle();
--- Aula 03/script.ts
+++ Aula 03/script.ts
@@ -1,6 +1,24 @@
+// EXERCICIO 1
+
 const link = document.getElementById("origamid")
 
 
 link instanceof HTMLAnchorElement ? link.href = link.href.replace('http', 'https') : ''
 
 console.log(link)
+
+
+// EXERCÍCIO 2
+
+const links = document.querySelectorAll<HTMLElement>('.link')
+
+const changeLinkStyle = () => {
+ const linksArray = Array.from(links) 
+  linksArray.forEach((link) => {
+    link.style.color = 'red'
+    link.style.border = '2px dotted tomato'
+
+  })
+}
+
+changeLinkStyle()

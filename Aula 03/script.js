99c3a16a461b99877526a84fdd6d6a6fb1d4a1f4
Felipe Manchester <f.p2809@gmail.com>
2023-01-27 12:11:06 -0300

    lesson 0303

--- Aula 03/script.js
+++ Aula 03/script.js
@@ -13,3 +13,31 @@ const changeLinkStyle = () => {
     });
 };
 changeLinkStyle();
+// EXERCÍCIO 3
+// Estado dos elementos
+// menu inativo:
+// class="" em nav
+// aria-expanded="false" em button
+// aria-label="Abrir Menu" em button
+// menu ativo:
+// class="active" em nav
+// aria-expanded="true" em button
+// aria-label="Fechar Menu" em button
+const nav = document.getElementById('nav');
+const button = document.getElementById('btn-mobile');
+function toggleMenu(event) {
+    const buttonMobile = event.currentTarget;
+    if (buttonMobile instanceof HTMLElement && nav) {
+        nav.classList.toggle('active');
+        const active = nav.classList.contains('active');
+        if (active) {
+            button?.setAttribute('aria-expanded', 'true');
+            button?.setAttribute('aria-label', 'Fechar Menu');
+        }
+        else {
+            button?.setAttribute('aria-expanded', 'false');
+            button?.setAttribute('aria-label', 'Abrir Menu');
+        }
+    }
+}
+button?.addEventListener('pointerdown', toggleMenu);

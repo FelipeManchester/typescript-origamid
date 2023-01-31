64045b3d0e8a5e1c3b5e5e3a21758ba3c8c95b30
Felipe Manchester <f.p2809@gmail.com>
2023-01-31 12:18:54 -0300

    lesson 0401

---
+++ Aula 04/script.ts
@@ -0,0 +1,28 @@
+// 1 - Faça um fetch das vendas: https://api.origamid.dev/json/vendas.json
+// 2 - Defina o tipo/interface de cada venda (tuple)
+// 3 - Some o total das vendas e mostre na tela
+
+interface DetalheVenda {
+  marca: string;
+  cor: string;
+}
+type Vendas = [string, number, string, DetalheVenda];
+
+async function fetchData() {
+  const res = await fetch ('https://api.origamid.dev/json/vendas.json')
+  const data = await res.json();
+  somarVendas(data)
+}
+
+const somarVendas = (vendas: Vendas[]) => {
+  const total = vendas.reduce((acc, cur) => {
+    return acc + cur[1]
+  }, 0)
+
+  document.body.innerHTML += `
+  <h1>Exercício 1</h1>
+  <p>Total de vendas: R$ ${total}
+  <hr>
+  `
+}
+fetchData()
